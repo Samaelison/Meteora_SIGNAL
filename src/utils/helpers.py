eed3af7893e28a21b199b1eb4c5f4f782e03e573
Samaelison <105775456+Samaelison@users.noreply.github.com>
2025-08-03 00:56:52 +0200

    Update helpers.py

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,4 +1,4 @@
-# E:\MeteoraMeme\meteora_bot\src\utils\helpers.py
+
 
 import yaml
 from pathlib import Path
@@ -9,8 +9,6 @@ def load_config() -> dict:
     Считывает config.yaml и возвращает словарь с настройками.
     """
     config_path = PROJECT_ROOT / "config" / "config.yaml"
-    # Пояснение: с помощью parent.parent.parent поднимаемся из src/utils/
-    # до корня проекта, затем идём в config/, берём config.yaml
     with open(config_path, "r", encoding="utf-8") as f:
         data = yaml.safe_load(f)
     return data
